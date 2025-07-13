d1a29c8fda16665b4659ffdbf56e903ead658bad
Hakim <hakimfidjel.pro@gmail.com>
2025-07-13 19:14:30 +0200

    [FIX] - Better sidebar

--- resources/js/components/layouts/sidebar/_sidebar.tsx
+++ resources/js/components/layouts/sidebar/_sidebar.tsx
@@ -31,145 +31,145 @@ import {
 	SidebarMenuItem,
 } from "@/components/ui/sidebar";
 
-const data = {
-	navigation: [
-		{
-			title: "Dashboard",
-			url: route("dashboard"),
-			icon: LayoutDashboard,
-			isActive: true,
-		},
-		{
-			title: "Projects",
-			url: route("projects.index"),
-			icon: Folder,
-			items: [
-				{
-					title: "Project 1",
-					url: "#",
-				},
-				{
-					title: "Project 2",
-					url: "#",
-				},
-				{
-					title: "Project 3",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Docker",
-			url: route("not-implemented"),
-			icon: Container,
-			items: [
-				{
-					title: "Containers",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Volumes",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Networks",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Images",
-					url: route("not-implemented"),
-				},
-			],
-		},
-		{
-			title: "Network & Security",
-			url: route("not-implemented"),
-			icon: Network,
-			items: [
-				{
-					title: "Activity",
-					url: route("not-implemented"),
-				},
-				{
-					title: "SSH Keys",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Traefik",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Firewall",
-					url: route("not-implemented"),
-				},
-			],
-		},
-		{
-			title: "Storage",
-			url: route("not-implemented"),
-			icon: Archive,
-			items: [
-				{
-					title: "Disk Usage",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Backups",
-					url: route("not-implemented"),
-				},
-			],
-		},
-		{
-			title: "Monitoring",
-			url: route("not-implemented"),
-			icon: SquareActivity,
-			items: [
-				{
-					title: "Processes",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Resources",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Containers",
-					url: route("not-implemented"),
-				},
-				{
-					title: "Logs",
-					url: route("not-implemented"),
-				},
-			],
-		},
-	],
-
-	footer: [
-		{
-			title: "Terminal",
-			url: route("not-implemented"),
-			icon: SquareTerminal,
-		},
-		{
-			title: "Account",
-			url: route("not-implemented"),
-			icon: User,
-		},
-		{
-			title: "Log out",
-			url: route("not-implemented"),
-			icon: LogOut,
-		},
-	],
-
-	user: {
-		name: "Hakim Fidjel",
-		email: "m@example.com",
-	},
-};
-
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	const [openMenu, setOpenMenu] = React.useState<string | null>(null);
+	const data = {
+		navigation: [
+			{
+				title: "Dashboard",
+				url: route("dashboard"),
+				icon: LayoutDashboard,
+				active: route().current("dashboard"),
+			},
+			{
+				title: "Projects",
+				url: route("projects.index"),
+				icon: Folder,
+				active: (route().current("projects.*") && !route().current("projects.show")),
+				items: [
+					{
+						title: "Project 1",
+						url: "#",
+						active: route().current("projects.show"),
+					},
+					{
+						title: "Project 2",
+						url: "#",
+					},
+					{
+						title: "Project 3",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Docker",
+				url: route("not-implemented"),
+				icon: Container,
+				items: [
+					{
+						title: "Containers",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Volumes",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Networks",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Images",
+						url: route("not-implemented"),
+					},
+				],
+			},
+			{
+				title: "Network & Security",
+				url: route("not-implemented"),
+				icon: Network,
+				items: [
+					{
+						title: "Activity",
+						url: route("not-implemented"),
+					},
+					{
+						title: "SSH Keys",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Traefik",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Firewall",
+						url: route("not-implemented"),
+					},
+				],
+			},
+			{
+				title: "Storage",
+				url: route("not-implemented"),
+				icon: Archive,
+				items: [
+					{
+						title: "Disk Usage",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Backups",
+						url: route("not-implemented"),
+					},
+				],
+			},
+			{
+				title: "Monitoring",
+				url: route("not-implemented"),
+				icon: SquareActivity,
+				items: [
+					{
+						title: "Processes",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Resources",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Containers",
+						url: route("not-implemented"),
+					},
+					{
+						title: "Logs",
+						url: route("not-implemented"),
+					},
+				],
+			},
+		],
+
+		footer: [
+			{
+				title: "Terminal",
+				url: route("not-implemented"),
+				icon: SquareTerminal,
+			},
+			{
+				title: "Account",
+				url: route("not-implemented"),
+				icon: User,
+			},
+			{
+				title: "Log out",
+				url: route("not-implemented"),
+				icon: LogOut,
+			},
+		],
+
+		user: {
+			name: "Hakim Fidjel",
+			email: "m@example.com",
+		},
+	};
 
 	return (
 		<Sidebar collapsible="icon" {...props}>
@@ -178,11 +178,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 					<SidebarMenuItem>
 						<SidebarMenuButton
 							asChild
-							className="data-[slot=sidebar-menu-button]:!py-1.5 !px-0 group-data-[collapsible=icon]:!p-0"
+							className="data-[slot=sidebar-menu-button]:!py-1.5 !px-0 group-data-[collapsible=icon]:!p-0 "
 						>
 							<Link href={route("dashboard")}>
 								<div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-									<GalleryVerticalEnd className="size-4 text-white" />
+									<span className="text-xl text-white italic font-bold">H</span>
 								</div>
 								<span className="text-base font-semibold">VPS Manager </span>
 							</Link>
@@ -192,18 +192,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 			</SidebarHeader>
 			<Separator />
 			<SidebarContent>
-				<NavLinks
-					title="Navigation"
-					items={data.navigation}
-					openMenu={openMenu}
-					setOpenMenu={setOpenMenu}
-				/>
-				<NavLinks
+				<NavLinks title="Navigation" items={data.navigation} />
+				{/* <NavLinks
 					title="Quick access"
 					items={data.footer}
-					openMenu={openMenu}
-					setOpenMenu={setOpenMenu}
-				/>
+				/> */}
 			</SidebarContent>
 			<Separator />
 			<SidebarFooter>
--- resources/js/components/layouts/sidebar/nav-links.tsx
+++ resources/js/components/layouts/sidebar/nav-links.tsx
@@ -1,103 +1,116 @@
 "use client";
 
-import { ChevronRight, type LucideIcon } from "lucide-react";
+// Necessary imports
+import { Link } from "@inertiajs/react";
 
-import { Link } from '@inertiajs/react';
-
+// Shadcn UI components
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 
 import {
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuAction,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-  useSidebar,
+	SidebarGroup,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuAction,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+	useSidebar,
 } from "@/components/ui/sidebar";
-// import Link from "next/link";
 
-
+// Icons
+import { ChevronRight, type LucideIcon } from "lucide-react";
 
 export function NavLinks({
-  title,
-  className,
-  items,
-  openMenu,
-  setOpenMenu,
+	title,
+	className,
+	items,
 }: {
-  title?: string;
-  className?: string;
-  items: {
-    title: string;
-    url: string;
-    icon: LucideIcon;
-    isActive?: boolean;
-    items?: { title: string; url: string }[];
-  }[];
-  openMenu: string | null;
-  setOpenMenu: (key: string | null) => void;
+	title?: string;
+	className?: string;
+	items: {
+		title: string;
+		url: string;
+		icon: LucideIcon;
+		active?: boolean;
+		items?: {
+			title: string;
+			url: string;
+			active?: boolean;
+		}[];
+	}[];
 }) {
-  const { openMenu: contextOpenMenu, setOpenMenu: contextSetOpenMenu } = useSidebar()
-  return (
-    <SidebarGroup className={className}>
-      {title && <SidebarGroupLabel>{title}</SidebarGroupLabel>}
+	const { openMenu: contextOpenMenu, setOpenMenu: contextSetOpenMenu } =
+		useSidebar();
 
-      <SidebarMenu>
-        {items.map((item) => {
-          const isOpen = contextOpenMenu === item.title;
+	return (
+		<SidebarGroup className={className}>
+			{title && <SidebarGroupLabel>{title}</SidebarGroupLabel>}
 
-          return (
-            <Collapsible
-              key={item.title}
-              open={isOpen}
-              onOpenChange={(open) => contextSetOpenMenu(open ? item.title : null)}
-              asChild
-            >
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild tooltip={item.title}>
-                  <Link href={item.url}>
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </Link>
-                </SidebarMenuButton>
+			<SidebarMenu>
+				{items.map((item) => {
 
-                {item.items?.length ? (
-                  <>
-                    <CollapsibleTrigger asChild>
-                      <SidebarMenuAction className="data-[state=open]:rotate-90">
-                        <ChevronRight />
-                        <span className="sr-only">Toggle</span>
-                      </SidebarMenuAction>
-                    </CollapsibleTrigger>
 
-                    <CollapsibleContent>
-                      <SidebarMenuSub>
-                        {item.items.map((subItem) => (
-                          <SidebarMenuSubItem key={subItem.title}>
-                            <SidebarMenuSubButton asChild>
-                              <Link href={subItem.url}>
-                                <span>{subItem.title}</span>
-                              </Link>
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                        ))}
-                      </SidebarMenuSub>
-                    </CollapsibleContent>
-                  </>
-                ) : null}
-              </SidebarMenuItem>
-            </Collapsible>
-          );
-        })}
-      </SidebarMenu>
-    </SidebarGroup>
-  );
+					const isOpen = contextOpenMenu === item.title;
+					const hasActiveSubItem = item.items?.some((subItem) => subItem.active);
+					const shouldBeOpen = isOpen || hasActiveSubItem;
+
+					return (
+						<Collapsible
+							key={item.title}
+							open={shouldBeOpen}
+							onOpenChange={(open) => contextSetOpenMenu(open ? item.title : null)}
+							asChild
+						>
+							<SidebarMenuItem>
+								<SidebarMenuButton
+									asChild
+									tooltip={item.title}
+									isActive={item.active}
+								>
+									<Link href={item.url}>
+										<item.icon />
+										<span>{item.title}</span>
+									</Link>
+								</SidebarMenuButton>
+
+								{item.items?.length ? (
+									<>
+										<CollapsibleTrigger asChild>
+											<SidebarMenuAction
+												className="data-[state=open]:rotate-90"
+												disabled={hasActiveSubItem}
+											>
+												<ChevronRight />
+												<span className="sr-only">Toggle</span>
+											</SidebarMenuAction>
+										</CollapsibleTrigger>
+
+										<CollapsibleContent>
+											<SidebarMenuSub>
+												{item.items.map((subItem) => (
+													<SidebarMenuSubItem key={subItem.title}>
+														<SidebarMenuSubButton asChild isActive={subItem.active}>
+															<Link href={subItem.url}>
+																<span>{subItem.title}</span>
+															</Link>
+														</SidebarMenuSubButton>
+													</SidebarMenuSubItem>
+												))}
+											</SidebarMenuSub>
+										</CollapsibleContent>
+									</>
+								) : null}
+							</SidebarMenuItem>
+						</Collapsible>
+					);
+				})}
+			</SidebarMenu>
+		</SidebarGroup>
+	);
 }
