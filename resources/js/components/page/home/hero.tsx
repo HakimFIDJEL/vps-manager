0b468ecca8d4511d22dc7cf86ee2443b2af38514
Hakim <hakimfidjel.pro@gmail.com>
2025-09-02 11:05:40 +0200

    [ADD] - Working on Hero page

--- resources/js/components/page/home/hero.tsx
+++ resources/js/components/page/home/hero.tsx
@@ -16,6 +16,7 @@ import {
 	Logs,
 	Zap,
 } from "lucide-react";
+import { SmoothItem } from "@/components/ui/smooth-resized";
 
 export function Hero() {
 	const { appearance } = useAppearance();
@@ -23,12 +24,12 @@ export function Hero() {
 	return (
 		<section className="relative py-36">
 			<div className="relative z-10 mx-auto w-full">
-				<div className="md:w-1/2">
+				<div className="md:w-[45%]">
 					<div>
-						<h1 className="max-w-md text-balance text-4xl font-base md:text-6xl">
+						<h1 className="max-w-sm text-balance text-4xl font-base md:text-5xl">
 							VPS Management Simplified
 						</h1>
-						<p className="text-muted-foreground my-8 max-w-2xl text-balance text-xl font-light">
+						<p className="text-muted-foreground my-8 max-w-2xl text-balance text-lg font-light">
 							Effortlessly orchestrate, monitor, and control all your VPS projects from
 							one platform.
 						</p>
@@ -68,34 +69,36 @@ export function Hero() {
 				</div>
 			</div>
 
-			<div className="perspective-near mt-24 translate-x-12 md:absolute md:-right-6 md:bottom-16 md:left-1/2 md:top-40 md:mt-0 md:translate-x-0 relative">
+			<div className="perspective-near mt-24 translate-x-12 md:absolute md:-right-6 md:bottom-16 md:left-[45%] md:top-40 md:mt-0 md:translate-x-0 relative">
 				<div className="before:border-foreground/5 before:bg-foreground/5 absolute before:absolute before:-inset-x-4 before:bottom-7 before:top-0 before:skew-x-6 before:rounded-[calc(var(--radius)+1rem)] before:border aspect-16/9 w-[40rem] top-1/2 translate-y-[-50%]">
-					<div className="bg-background rounded-(--radius) shadow-foreground/10 ring-foreground/5 relative h-full -translate-y-12 skew-x-6 overflow-hidden border border-transparent shadow-md ring-1 ">
-						{appearance === "dark" && (
-							<img
-								src="assets/images/hero-dark.png"
-								className="object-top-left size-full object-cover"
-							/>
-						)}
+					<SmoothItem delay={0.5} layout={false}>
+						<div className="bg-background rounded-(--radius) shadow-foreground/10 ring-foreground/5 relative h-full -translate-y-12 skew-x-6 overflow-hidden border border-transparent shadow-md ring-1 ">
+							{appearance === "dark" && (
+								<img
+									src="assets/images/hero-dark.png"
+									className="object-top-left size-full object-cover"
+								/>
+							)}
 
-						{appearance === "light" && (
-							<img
-								src="assets/images/hero-light.png"
-								className="object-top-left size-full object-cover"
-							/>
-						)}
+							{appearance === "light" && (
+								<img
+									src="assets/images/hero-light.png"
+									className="object-top-left size-full object-cover"
+								/>
+							)}
 
-						{appearance === "system" && (
-							<img
-								src={
-									window.matchMedia("(prefers-color-scheme: dark)").matches
-										? "assets/images/hero-dark.png"
-										: "assets/images/hero-light.png"
-								}
-								className="object-top-left size-full object-cover"
-							/>
-						)}
-					</div>
+							{appearance === "system" && (
+								<img
+									src={
+										window.matchMedia("(prefers-color-scheme: dark)").matches
+											? "assets/images/hero-dark.png"
+											: "assets/images/hero-light.png"
+									}
+									className="object-top-left size-full object-cover"
+								/>
+							)}
+						</div>
+					</SmoothItem>
 				</div>
 			</div>
 		</section>
