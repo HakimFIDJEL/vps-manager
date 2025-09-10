17381f198ab3f6dee46f55655c511842d3622e94
Hakim <hakimfidjel.pro@gmail.com>
2025-09-10 10:40:37 +0200

    [DEBUG]

--- app/Services/Authentication.php
+++ app/Services/Authentication.php
@@ -29,7 +29,7 @@ class Authentication
      */
     public function authenticate(string $username, string $password): array
     {
-        $cmd = escapeshellarg($this->pythonPath) . ' ' . escapeshellarg($this->scriptsPath) . ' ' . escapeshellarg($username);
+        $cmd = 'sudo ' . escapeshellarg($this->pythonPath) . ' ' . escapeshellarg($this->scriptsPath) . ' ' . escapeshellarg($username);
 
         $pipes = [];
         $process = proc_open($cmd, [
