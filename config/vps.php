cce151864a973b701cd3b7890ececac041743462
Hakim <hakimfidjel.pro@gmail.com>
2025-09-15 16:24:16 +0200

    [DEBUG]

--- config/vps.php
+++ config/vps.php
@@ -1,11 +1,6 @@
 <?php
 
-// return [
-//     'python_path' => env('PYTHON_PATH', '/usr/bin/python3'),
-//     'scripts_path' => base_path('scripts') . '/authenticate.py',
-// ];
-
 return [
-  'python_path' => '/var/www/html/.venv/bin/python',  
-  'scripts_path' => '/var/www/html/scripts/authenticate.py',
+    'python_path' => env('PYTHON_PATH', '/usr/bin/python3'),
+    'scripts_path' => base_path('scripts') . '/authenticate.py',
 ];
