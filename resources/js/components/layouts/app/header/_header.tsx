ed803d3d8279230bef8d853ba4031444d26f0678
Hakim <hakimfidjel.pro@gmail.com>
2025-09-20 16:32:02 +0200

    [FIX] - Added header in policies layout

--- resources/js/components/layouts/app/header/_header.tsx
+++ resources/js/components/layouts/app/header/_header.tsx
@@ -4,7 +4,7 @@ import { Link } from "@inertiajs/react";
 // Custom components
 import { Breadcrumbs } from "@/components/layouts/app/header/breadcrumbs";
 import AppearanceToggleDropdown from "@/components/layouts/app/header/theme";
-import { Logo } from "../../logo";
+import { Logo } from "../logo";
 
 // Shadcn UI components
 import { Separator } from "@/components/ui/separator";
--- resources/js/components/layouts/policies/header.tsx
+++ resources/js/components/layouts/policies/header.tsx
@@ -1,23 +1,14 @@
 // components/layouts/policies/header.tsx
 
-// Necessary imports
-import { Link } from "@inertiajs/react";
-
-// Shadcn UI Components
-import { Button } from "@/components/ui/button";
-
-// Icons
-import { ArrowLeft } from "lucide-react";
+// Custom Components
+import AppearanceToggleDropdown from "@/components/layouts/app/header/theme";
+import { Logo } from "@/components/layouts/app/logo";
 
 export function Header() {
-    return (
-        <nav>
-            <Button asChild variant={"link"} className="text-foreground !px-0">
-                <Link href={route('home')}>
-                    <ArrowLeft />
-                    Go back to home
-                </Link>
-            </Button>
-        </nav>
-    )
+	return (
+		<header className="bg-background/50 lg:bg-transparent fixed top-0 lg:backdrop-blur-none backdrop-blur-sm z-10 w-full flex shrink-0 items-center gap-2 border-b lg:border-b-0 justify-between px-6 py-4 lg:pointer-events-none">
+			<Logo variant={"mini"} color_scheme={"white"} />
+			<AppearanceToggleDropdown className="lg:pointer-events-auto" />
+		</header>
+	);
 }
